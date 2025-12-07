3337c6f60a1a89779f255347fd2b37f5367435fb
Euchie Jn Pierre <113702536+Euchie23@users.noreply.github.com>
2025-12-07 18:12:52 -0400

    Update preprocess.py

--- App/AutoML/utils/preprocess.py
+++ App/AutoML/utils/preprocess.py
@@ -5,18 +5,18 @@ BASE_DIR = os.path.dirname(__file__)  # points to App/AutoML
 DATA_PATH = os.path.join(BASE_DIR, "data", "weekly_processed.csv")
 
 def load_weekly_dataset():
-"""Loads your cleaned dataset."""
-     return pd.read_csv(DATA_PATH)
+    """Loads your cleaned dataset."""
+    return pd.read_csv(DATA_PATH)
 
 # FINAL_FEATURES = [
-# "Year", "WeekOfYear",
-# "Avg_weekly_Lat", "Avg_weekly_Lon",
-# "Avg_weekly_WaterTemp_2wk", "Avg_weekly_WaterTemp_3wk",
-# "Avg_weekly_Depth_2wk", "Avg_weekly_Depth_3wk",
-# "Chlor_a_mg_m3_5wk", "SSH_5wk",
-# "sin_week", "cos_week",
-# "Temp_x_Depth", "Temp_x_Chlor",
-# "Depth_x_Chlor", "Temp_x_SSH"
+#     "Year", "WeekOfYear",
+#     "Avg_weekly_Lat", "Avg_weekly_Lon",
+#     "Avg_weekly_WaterTemp_2wk", "Avg_weekly_WaterTemp_3wk",
+#     "Avg_weekly_Depth_2wk", "Avg_weekly_Depth_3wk",
+#     "Chlor_a_mg_m3_5wk", "SSH_5wk",
+#     "sin_week", "cos_week",
+#     "Temp_x_Depth", "Temp_x_Chlor",
+#     "Depth_x_Chlor", "Temp_x_SSH"
 # ]
 
 def build_feature_row(df_weekly, week_index, overrides=None):
@@ -35,3 +35,4 @@ def build_feature_row(df_weekly, week_index, overrides=None):
     row["Temp_x_SSH"]   = row["Avg_weekly_WaterTemp_3wk"] * row["SSH_5wk"]
 
     return row[FINAL_FEATURES].to_frame().T
+
