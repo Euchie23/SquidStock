c5f54cd412fe329c810d216c5783cf020c71aaa9
Euchie Jn Pierre <113702536+Euchie23@users.noreply.github.com>
2025-12-07 18:18:41 -0400

    Update app.py

--- App/AutoML/app.py
+++ App/AutoML/app.py
@@ -40,6 +40,9 @@ import streamlit as st
 # =========================================================
 warnings.filterwarnings("ignore")
 
+import os
+print("Current working dir:", os.getcwd())
+print("Files in data folder:", os.listdir(DATA_DIR))
 
 # ============================
 # 2️⃣ CUSTOM CSS 
