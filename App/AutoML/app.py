0f25227219aa634c4fdc2fca1110e781e022c3bf
Euchie Jn Pierre <113702536+Euchie23@users.noreply.github.com>
2026-03-10 23:32:46 -0400

    Update app.py

--- App/AutoML/app.py
+++ App/AutoML/app.py
@@ -274,22 +274,22 @@ p span.emoji {
 
 # /* ---------------------- Sidebar + Main Layout Fix ---------------------- */
 
-# header[data-testid="stHeader"] {
-#     display: none !important;
-# }
+header[data-testid="stHeader"] {
+    display: none !important;
+}
 
-# [data-testid="stToolbar"] {
-#     display: none !important;
-# }
+[data-testid="stToolbar"] {
+    display: none !important;
+}
 
-# [data-testid="stSidebar"] {
-#     top: 0 !important;
-#     height: 100vh !important;
-# }
+[data-testid="stSidebar"] {
+    top: 0 !important;
+    height: 100vh !important;
+}
 
-# div[data-testid="stAppViewContainer"] {
-#     margin-top: 0 !important;
-# }
+div[data-testid="stAppViewContainer"] {
+    margin-top: 0 !important;
+}
 
 # /* Sidebar fixed below top bar */
 # [data-testid="stSidebar"] {
