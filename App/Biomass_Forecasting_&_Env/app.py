499159efe142b63d1c381209adb9cfac4424e428
Euchie Jn Pierre <113702536+Euchie23@users.noreply.github.com>
2026-03-13 00:36:16 -0400

    Update app.py

--- App/Biomass_Forecasting_&_Env/app.py
+++ App/Biomass_Forecasting_&_Env/app.py
@@ -416,7 +416,7 @@ a[data-testid="stLinkButton"]:hover {
     box-sizing: border-box !important;
 
     /* occupy full width minus sidebar width */
-    width: calc(100vw - 300px) !important;
+    width: calc(100vw - 320px) !important;
 }
 
 /* ---------------------- Adjust when sidebar collapses ---------------------- */
