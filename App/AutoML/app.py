670545fc8ac4226e0f823b729eb1689fbca1470c
Euchie Jn Pierre <113702536+Euchie23@users.noreply.github.com>
2026-03-31 11:44:31 -0400

    Update app.py

--- App/AutoML/app.py
+++ App/AutoML/app.py
@@ -318,14 +318,13 @@ div[data-testid="stSlider"] label p {
     box-sizing: border-box !important;
 
     /* occupy full width minus sidebar width */
-    width: calc(100vw - 470px) !important;
+    width: calc(100vw - 430px) !important;
 }
 
 /* ---------------------- Adjust when sidebar collapses ---------------------- */
 
 [data-testid="stSidebar"][aria-expanded="false"] ~ div .block-container {
-    /* width: calc(100vw - 80px) !important;*/
-    margin-left: 80px !important;
+    width: calc(100vw - 80px) !important;
 }
 
 /* ---------------------- Inner content wrapper fix ---------------------- */
