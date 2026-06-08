7536ccd90fe5427aedd137f2c8e34bf4d244261d
Euchie Jn Pierre <113702536+Euchie23@users.noreply.github.com>
2026-06-08 18:38:29 -0400

    Fix path resolution in load_weekly_dataset function

--- App/AutoML/utils/preprocess.py
+++ App/AutoML/utils/preprocess.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def load_weekly_dataset():
     # __file__ is preprocess.py
-    root = Path(__file__).parents[1]  # goes up 2 levels: utils -> AutoML
+    root = Path(__file__).resolve.parents[1]  # goes up 2 levels: utils -> AutoML
     data_path = root / "data" / "weekly_processed.csv"
     return pd.read_csv(data_path)
 
